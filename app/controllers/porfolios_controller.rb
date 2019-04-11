77468d875e2937476d531cdb65ff22c48a1645f2
Valter Fernandes <valterfernandes.dev@gmail.com>
2019-04-11 17:30:46 -0300

    dfsdfsd

--- app/controllers/porfolios_controller.rb
+++ app/controllers/porfolios_controller.rb
@@ -2,4 +2,21 @@ class PorfoliosController < ApplicationController
   def index
     @porfolio_items = Porfolio.all
   end
+
+  def new
+    @porfolio_item = Porfolio.new
+  end
+
+  def create
+    @portfolio_item = Porfolio.new(params.require(:porfolio).permit(:title, :subtitle, :body))
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to porfolios_path, notice: 'Your portfolio item is now live.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
 end
